64a7de5e7d87e5012b7c038df6e987b49419548b
Sandeepsingh <sandeepsinghrathorerajpoot@gmail.com>
2025-09-04 10:30:05 +0530

    modified json

--- Contacts_List/vite.config.js
+++ Contacts_List/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/Contacts_List/',
+  base: '/Contacts_List/',   // 👈 repo ka exact naam
 })
